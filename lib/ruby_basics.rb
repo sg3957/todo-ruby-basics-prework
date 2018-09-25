d77141c4bacf3fdb0f8e160bb582d4f36192925c
Sydney Grant <sg3957@nyu.edu>
2018-09-25 01:15:08 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -11,9 +11,49 @@ def argue(phrase)
   return phrase
 end
 
+
 def greeting
 
 end
+
+describe '#greeting' do
+    it 'takes two arguments' do
+      greeting = "Hi there, "
+      name = "Bobby!"
+
+      expect{ greeting(greeting, name) }.to_not raise_error
+    end
+
+    it 'raises an error with one argument' do
+      greeting = "Hi there, "
+
+      expect{ greeting(greeting)}.to raise_error
+    end
+  end
+
+  describe '#return_a_value' do
+    it 'returns the phrase "Nice"' do
+      expect(return_a_value).to eq("Nice")
+    end
+  end
+
+  describe '#last_evaluated_value' do
+    it 'returns the phrase "expert"' do
+      expect(last_evaluated_value).to eq("expert")
+    end
+  end
+
+  describe '#pizza_party' do
+    it 'returns "cheese" by default' do
+      expect(pizza_party).to eq("cheese")
+    end
+
+    it 'returns the argument it received' do
+      expect(pizza_party("pepperoni")).to eq("pepperoni")
+    end
+  end
+end
+
 
 def return_a_value
 
